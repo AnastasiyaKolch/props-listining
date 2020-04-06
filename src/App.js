97adda51606c5d9cf28630f4bd5ff4d2e70888f0
AnastasiyaKolch <45988412+AnastasiyaKolch@users.noreply.github.com>
2020-04-06 19:53:01 +0500

    +

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './main.css';
+import Listing from './components/Listing';
+import {etsy} from './components/data'
+
+function App() {
+  const arr = []
+  etsy.map(el => {
+    const {listing_id, url, MainImage, title, currency_code, price, quantity, category_id} = el
+    return arr.push({listing_id, url, MainImage, title, currency_code, price, quantity, category_id});
+  })
+
+  console.log(arr)
+  const data = arr.filter(el => el.MainImage !== undefined )
+
+  return (
+      <div className="item-list"> 
+        <Listing data={data} />
+      </div>
+  )
+}
+
+export default App;
